1b3c1af479cbf34d2de390c12e78b28808d24bf3
gdc-agency <info.globaldigitalcare@gmail.com>
2026-04-24 18:14:08 +0500

    Update footer link to use handleOpenNewtab

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,12 +1,18 @@
 import { currentYear, developedBy } from '@/context/constants';
 import { Col, Container, Row } from 'react-bootstrap';
 const Footer = () => {
+  const handleOpenNewtab = (url) => {
+  window.open(url, '_blank', 'noopener,noreferrer');
+};
   return <footer className="footer">
       <Container fluid>
         <Row>
           <Col xs={12} className="text-center">
             {currentYear} © Techmin - Theme by <Col xs={12} className="text-center">
-          {currentYear} © Techmin - Theme by <a href="" onClick={() => handleOpenNewtab("https://www.globaldigitalcare.com/")} target="_blank" rel="noopener noreferrer">Global Digital Care</a>
+          {currentYear} © Techmin - Theme by <a href="" onClick={(e) => {
+    e.preventDefault();
+    handleOpenNewtab("https://www.globaldigitalcare.com/");
+  }} target="_blank" rel="noopener noreferrer">Global Digital Care</a>
         </Col>
           </Col>
         </Row>
